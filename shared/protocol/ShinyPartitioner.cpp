d0009884bd15178a6af824d660ffbcb612be3b3e
Elliot Saba <staticfloat@gmail.com>
2011-08-14 23:39:56 -0700

    Beta release for ShinyPartitioner; passes all tests right now.  Formal testing yet to begin.

--- shared/protocol/ShinyPartitioner.cpp
+++ shared/protocol/ShinyPartitioner.cpp
@@ -1,31 +1,6 @@
 #include "ShinyPartitioner.h"
 #include "base/Logger.h"
 
-void buildWeightMap( ShinyMetaFilesystem * fs, ShinyMetaDir *root, std::map<inode_t, uint64_t> * weightMap ) {
-    //Traverse through the fs, recursing on directories, summing weights into rootWeight as we go
-    uint64_t rootWeight = 0;
-    
-    const std::list<inode_t> * children = root->getListing();
-    for( std::list<inode_t>::const_iterator itty = children->begin(); itty != children->end(); ++itty ) {
-        //Even though this shouldn't happen, I'd rather error on the side of safety and do a NULL check
-        ShinyMetaNode * child = fs->findNode(*itty);
-        if( child && child->getPrimaryParent()->getInode() == root->getInode() ) {
-            //Only do this if we are the primary parent
-            if( child->getNodeType() == SHINY_NODE_TYPE_FILE) {
-                //We always asssign 1 here, which is possibl e a little naive.  We should profile activity on files and assign weights accordingly MAYBE
-                (*weightMap)[*itty] = 1;
-            } else if( child->getNodeType() == SHINY_NODE_TYPE_DIR || child->getNodeType() == SHINY_NODE_TYPE_ROOTDIR ) {
-                //There's no real reason I should ever have a ROOTDIR as a child of another dir, but it makes "sense"
-                //that I would want this to still happen if I did, so I'm putting it in there-STOP LOOKING AT ME LIKE THAT
-                buildWeightMap( fs, (ShinyMetaDir *)child, weightMap );
-            }
-            
-            //After those two things, update the root weight
-            rootWeight += (*weightMap)[*itty];
-        }
-    }
-    (*weightMap)[root->getInode()] = rootWeight;
-}
 
 //Helper class to sort inodes based on their weight in the weightMap
 struct WeightSorter {
@@ -37,41 +12,71 @@ struct WeightSorter {
     std::map<inode_t, uint64_t> weightMap;
 };
 
-std::map< std::list<inode_t> *, std::list<ShinyPeer *> *> * partitionRegion( ShinyMetaFilesystem * fs, std::list<inode_t> * rootRegion, std::list<ShinyPeer *> * peers, ShinyPeer * us, std::map<inode_t, uint64_t> * weightMap, uint64_t weightPerPeer ) {
-    bool shouldFreeWeightMap = false;
-    //Find the root
+ShinyPartitioner::ShinyPartitioner( ShinyMetaFilesystem *fs, std::list<inode_t> *rootRegion, std::list<ShinyPeer *> *peers, ShinyPeer * us ) : fs( fs ), us( us ) {
+    //First things first, get the root of the filesystem;
     ShinyMetaDir * root = (ShinyMetaDir *)fs->findNode( "/" );
-
-    //This means its the first iteration
-    if( !weightMap ) {
-        weightMap = new std::map<inode_t, uint64_t>();
-        //Begin by fleshing out the weight map along each directory, etc.
-        buildWeightMap( fs, root, weightMap );
-    
-        //Calculate the ideal weight per peer
-        weightPerPeer = (uint64_t)ceil( (double)(*weightMap)[root->getInode()]/peers->size() );
+    if( !root ) {
+        ERROR( "Could not find root node!" );
+        return;
     }
+    
+    //Build the weight map for the entire FS:
+    this->buildWeightMap( root );
+    //this->printWeightMap( root );
+    
+    //Calculate ideal weight per peer
+    uint64_t rootWeight = 0;
+    for( std::list<inode_t>::iterator itty = rootRegion->begin(); itty != rootRegion->end(); ++itty )
+        rootWeight += weightMap[*itty];
+    this->weightPerPeer = (uint64_t)ceil( (double)rootWeight/(peers->size()+1) );
+    
+    //Finally, start partitioning out!
+    this->regionPeers_final = this->partitionRegion( rootRegion, peers );
+    
+    //Find "our" region, and construct the list of "our" children
+    ourChildren_final = new std::list<ShinyPeer *>();
+    for( std::map< std::list<inode_t> *, std::list<ShinyPeer *> >::iterator itty = regionPeers_final->begin(); itty != regionPeers_final->end(); ++itty ) {
+        if( (*itty).second.front() == us )
+            ourRegion_final = (*itty).first;
+        else
+            ourChildren_final->push_back( (*itty).second.front() );
+    }
+}
+
+ShinyPartitioner::~ShinyPartitioner() {
+    delete( this->ourChildren_final );
+    for( std::map< std::list<inode_t> *, std::list<ShinyPeer *> >::iterator itty = this->regionPeers_final->begin(); itty != this->regionPeers_final->end(); ++itty )
+        delete( (*itty).first );
+    delete( this->regionPeers_final );
+}
+
+std::map< std::list<inode_t> *, std::list<ShinyPeer *> > * ShinyPartitioner::partitionRegion( std::list<inode_t> * rootRegion, std::list<ShinyPeer *> * peers, uint64_t initialWeight ) {
+    printf( "Our root region:\n" );
+    for( std::list<inode_t>::iterator itty = rootRegion->begin(); itty != rootRegion->end(); ++itty )
+        printf("[%llu] %s\n", *itty, fs->findNode( (*itty) )->getName() );
     
     //This maps our lists of inodes to the (list of) peers that own them
     std::map< std::list<inode_t> *, uint64_t> regionSizes;
     
-    //First, we know that we shall own every inode in root region.  We will also own children until we are full;
+    //The list of indoes belonging to each new region we will create
     std::list<inode_t> * newRegion = new std::list<inode_t>();
+    //The weight of "newRegion", aka "what is assigned to us"
+    uint64_t regionWeight = initialWeight;
     
-    //Save the first region we make, as that region contains "us" and will be checked to make sure it doesn't need to be recursed into
+    //Save the first region we make as ourRegion, as that region contains "us" and will be checked for directories (and most importantly, other peers)
     std::list<inode_t> * ourRegion = newRegion;
-    uint64_t regionWeight = 0;
+    
+    //First, we know that we shall own every inode in the root region.  We will also start owning children until we are full;    
     for( std::list<inode_t>::iterator itty = rootRegion->begin(); itty != rootRegion->end(); ++itty ) {
-        newRegion->push_front( *itty );
-        
         //Only add on the weight if this node is actually a file, as we don't count the weights
         //of the directories on the top level
         ShinyMetaNode * inode = fs->findNode( *itty );
-        if( inode && inode->getNodeType() == SHINY_NODE_TYPE_FILE ) {
-            newRegion->push_back( *itty );
-            regionWeight += (*weightMap)[*itty];
-        }
+        if( inode && inode->getNodeType() == SHINY_NODE_TYPE_FILE )
+            regionWeight += this->weightMap[*itty];
     }
+    
+    //Save this, as we'll use it later when recursing
+    uint64_t rootWeight = regionWeight;
     
     //Now iterate through all children, starting from the smallest to the largest dirs in the root region.
     //As we iterate, fill up a new region until the weight of that region is greater than or equal to the
@@ -82,17 +87,21 @@ std::map< std::list<inode_t> *, std::list<ShinyPeer *> *> * partitionRegion( Shi
         if( inode && (inode->getNodeType() == SHINY_NODE_TYPE_DIR || inode->getNodeType() == SHINY_NODE_TYPE_ROOTDIR) ) {
             const std::list<inode_t> * children = ((ShinyMetaDir *)inode)->getListing();
             std::list<inode_t> sortedChildren( *children );
-            sortedChildren.sort( WeightSorter( *weightMap ) );
+            sortedChildren.sort( WeightSorter( weightMap ) );
+            
+//            printf( "sorted children of %s:\n", inode->getName() );
+//            for( std::list<inode_t>::iterator sitty = sortedChildren.begin(); sitty != sortedChildren.end(); ++sitty )
+//                printf( "[%llu] %s\n", weightMap[*sitty], fs->findNode( *sitty)->getName() );
             
             //Now iterate over the children that have been sorted by weight
             for( std::list<inode_t>::iterator child_itty = sortedChildren.begin(); child_itty != sortedChildren.end(); ++child_itty ) {
                 //This time around, we _will_ add in weights for the directories, so don't check for type
-                regionWeight += (*weightMap)[*child_itty];
+                regionWeight += this->weightMap[*child_itty];
                 newRegion->push_back( *child_itty );
                 
                 //If we have more than the threshold of weight, then we need to define this list of inodes as its own region
                 if( regionWeight >= weightPerPeer ) {
-                    regionSizes[newRegion] = regionWeight/weightPerPeer;
+                    regionSizes[newRegion] = roundf((float)regionWeight/weightPerPeer);
                     newRegion = new std::list<inode_t>();
                     regionWeight = 0;
                 }
@@ -100,60 +109,175 @@ std::map< std::list<inode_t> *, std::list<ShinyPeer *> *> * partitionRegion( Shi
         }
     }
     
+    if( newRegion->size() )
+        regionSizes[newRegion] = regionWeight;
+    else if( newRegion != ourRegion )
+        delete( newRegion );
+    
     //Now we have a list of regions along with the number of nodes in each region.  Let's sort the regions by number of peers
-    //assigned to each region, and then dole out the peers round-robin style.
+    //assigned to each region:
     std::list< std::list<inode_t> *> sortedRegions;
     //Basic insertion sort:
     for( std::map< std::list<inode_t> *, uint64_t>::iterator map_itty = regionSizes.begin(); map_itty != regionSizes.end(); ++map_itty ) {
-        for( std::list< std::list<inode_t> *>::iterator list_itty = sortedRegions.begin(); list_itty != sortedRegions.end(); ++list_itty ) {
+//        for( std::list<inode_t>::iterator itty = (*map_itty).first->begin(); itty != (*map_itty).first->end(); ++itty )
+//            printf( "[%llu] %s\n", *itty, fs->findNode( (*itty) )->getName() );
+        std::list< std::list<inode_t> *>::iterator list_itty;
+        for( list_itty = sortedRegions.begin(); list_itty != sortedRegions.end(); ++list_itty ) {
             //Insert before everything smaller than us, so that we go in descending order
-            if( regionSizes[*list_itty] < (*map_itty).second ) {
-                sortedRegions.insert(list_itty, (*map_itty).first );
+            if( regionSizes[*list_itty] < (*map_itty).second )
                 break;
+        }
+        sortedRegions.insert(list_itty, (*map_itty).first );
+    }
+    
+    //Now, dole out peers round-robin style amongst all the regions: (but only if we _have_ regions, LOL)
+    std::map< std::list<inode_t> *, std::list<ShinyPeer *> > * regionPeers = new std::map< std::list<inode_t> *, std::list<ShinyPeer *> >();
+    if( !sortedRegions.empty() ) {
+        std::list<std::list<inode_t> *>::iterator region_itty = sortedRegions.begin();
+        
+        //Push ourselves onto the very end, so that we can put ourselves on when we hit "our" region;
+        peers->push_back( us );
+        uint64_t numPeersLeft = peers->size();
+        while( !peers->empty() ) {
+            if( (*regionPeers)[*region_itty].size() < regionSizes[*region_itty] ) {
+                //Special case for "our" region; if it's empty, put us in first!
+                if( *region_itty == ourRegion && (*regionPeers)[*region_itty].empty() ) {
+                    (*regionPeers)[*region_itty].push_back( us );
+                    peers->remove( us );
+                }
+                else {
+                    (*regionPeers)[*region_itty].push_back( peers->front() );
+                    peers->pop_front();
+                }
+            }
+            printf("%d < %d\n", (*regionPeers)[*region_itty].size(), regionSizes[*region_itty] );
+            ++region_itty;
+            if( region_itty == sortedRegions.end() ) {
+                //Check to make sure that we made any progress at all this last run through.  If we didn't RAGEQUIT
+                if( numPeersLeft == peers->size() )
+                    break;
+                //If we did, mark that progress and wait until next time.....
+                numPeersLeft = peers->size();
+                region_itty = sortedRegions.begin();
             }
         }
     }
-    
-    //Now, dole out peers:
 
     //Now check for the region with "us" in it, if it has a directory in it, we need to recursively call into it until "we" are alone
-    return NULL;
-}
-
-
-
-/*
-void ShinyPartitioner::partitionWeightMap( std::list<inode_t> * rootRegion, std::list<ShinyPeer *> *peers ) {
-    //This will sort the children of root such that the "lightest" files are at the beginning;
-    std::list<inode_t> sortedRegion( *rootRegion );
-    sortedRegion.sort( WeightSorter( &this->weightMap ) );
-    
-    //I save this guy off to the side, as he is the peer that takes the flack for all of the directories in this region
-    ShinyPeer * masterOfTheHouse = peers->front();
-    
-    //This is a list of inodes, comprising the current "region"
-    //std::list<inode_t> * currRegion = new std::list<inode_t>();
-    
-    for( std::list<inode_t>::const_iterator itty = sortedRegion.begin(); itty != sortedRegion.end(); ++itty ) {
-        LOG( "%d", weightMap[*itty] );
-
-        //Look at this member of the region
-        ShinyMetaNode * member = this->fs->findNode( *itty );
-        if( member && member->getPrimaryParent() ) {
-            //If it's a file, add it onto the responsibility of the top peer of [peers]
-            //Also, sum its weight into that peer's assignedWeights, and if he has too much, either split him off,
-            //or make a new region of him and a few buddoes
-            if( member->getNodeType() == SHINY_NODE_TYPE_FILE ) {
-                peerAssignments[peers->front()].push_back( *itty );
-                assignedWeights[peers->front()] += weightMap[*itty];
-                
-                if( assignedWeights[peers->front()] > this->weightPerNode )
-                    peers->
-            }
-            //If it's a dir, by default it belongs to the first peer, as that peer is MASTER 'O DE 'OUSE
-            if( member->getNodeType() == SHINY_NODE_TYPE_DIR || member->getNodeType() == SHINY_NODE_TYPE_ROOTDIR ) {
-                peerAssignments[masterOfTheHouse].push_back( *itty );
-            }
+    bool hasAnyDirectories = false;
+    for( std::list<inode_t>::iterator itty = ourRegion->begin(); itty != ourRegion->end(); ++itty ) {
+        ShinyMetaNode * node = fs->findNode( *itty );
+        if( node && node->getNodeType() == SHINY_NODE_TYPE_DIR ) {
+            hasAnyDirectories = true;
+            break;
         }
     }
-}*/
+
+    //If there are directories in here, then recurse and pass in the peers assigned to us;
+    if( hasAnyDirectories ) {        
+        uint64_t ourWeight = rootWeight;
+        for( std::list<inode_t>::iterator itty = ourRegion->begin(); itty != ourRegion->end(); ++itty ) {
+            ShinyMetaNode * node = fs->findNode( *itty );
+            if( node && node->getNodeType() == SHINY_NODE_TYPE_FILE ) {
+                printf( "Adding on %llu for [%s]\n", weightMap[*itty], fs->findNode(*itty)->getName() );
+                ourWeight += weightMap[*itty];
+            }
+        }
+        
+        //pop off ourself from this region so that we don't get added in twice to the list of peers
+        (*regionPeers)[ourRegion].pop_front();
+        
+        //Get all the subregion assignments
+        std::map< std::list<inode_t> *, std::list<ShinyPeer *> > * subRegionPeers = partitionRegion( ourRegion, &(*regionPeers)[ourRegion], rootWeight );
+        
+//        //put ourselves back in
+//        (*regionPeers)[ourRegion].push_front( us );
+                
+        //Now, take all the regions that have us (and only us) in them, and glob them together into one ginormous region
+        for( std::map< std::list<inode_t> *, std::list<ShinyPeer *> >::iterator itty = subRegionPeers->begin(); itty != subRegionPeers->end(); ++itty ) {
+            //If it's us, then merge it into "ourRegion"
+            if( (*itty).second.front() == us ) {
+                ourRegion->clear();
+                ourRegion->merge( *(*itty).first );
+/*                
+                printf("Post-merge:\n");
+                for( std::list<inode_t>::iterator inode_itty = ourRegion->begin(); inode_itty != ourRegion->end(); ++inode_itty ) {
+                    printf( "[%llu] %s\n", *inode_itty, fs->findNode( *inode_itty )->getPath() );
+                }*/
+            } else {
+                //Otherwise, merge this region into the big picture
+                (*regionPeers)[(*itty).first].merge( (*itty).second );
+            }
+//            delete( (*itty).first );
+        }
+        delete( subRegionPeers );
+    }
+    
+    //This covers two cases; first, if there were other people assigned to this region, it should now
+    //be only ourselves because we split our region off to other places.  Second, if there is
+    //only the "root" region, (e.g. no directories in root) then this will ensure that "ourRegion"
+    //gets added to regionPeers
+    (*regionPeers)[ourRegion].clear();
+    (*regionPeers)[ourRegion].push_front( us );
+
+    
+    //Add all the root region inodes in at the very end, so that none of it interferes with our stuffage
+    for( std::list<inode_t>::iterator itty = rootRegion->begin(); itty != rootRegion->end(); ++itty )
+        ourRegion->push_back( *itty );
+
+    
+    //And finally, return the whole map.  ^_^
+    return regionPeers;
+}
+
+void ShinyPartitioner::buildWeightMap( ShinyMetaDir *root ) {
+    //Traverse through the fs, recursing on directories, summing weights into rootWeight as we go
+    uint64_t rootWeight = 0;
+    
+    const std::list<inode_t> * children = root->getListing();
+    for( std::list<inode_t>::const_iterator itty = children->begin(); itty != children->end(); ++itty ) {
+        //Even though this shouldn't happen, I'd rather error on the side of safety and do a NULL check
+        ShinyMetaNode * child = fs->findNode(*itty);
+        if( child ) {
+            //Only do this if we are the primary parent
+            if( child->getNodeType() == SHINY_NODE_TYPE_FILE) {
+                //We always asssign 1 here, which is possibl e a little naive.  We should profile activity on files and assign weights accordingly MAYBE
+                this->weightMap[*itty] = 1;
+            } else if( child->getNodeType() == SHINY_NODE_TYPE_DIR || child->getNodeType() == SHINY_NODE_TYPE_ROOTDIR ) {
+                //There's no real reason I should ever have a ROOTDIR as a child of another dir, but it makes "sense"
+                //that I would want this to still happen if I did, so I'm putting it in there-STOP LOOKING AT ME LIKE THAT
+                buildWeightMap( (ShinyMetaDir *)child );
+            }
+            
+            //After those two things, update the root weight
+            rootWeight += this->weightMap[*itty];
+        }
+    }
+    this->weightMap[root->getInode()] = rootWeight;
+}
+
+void ShinyPartitioner::printWeightMap( ShinyMetaDir * root, uint64_t depth ) {
+    const std::list<inode_t> * children = root->getListing();
+    for( std::list<inode_t>::const_iterator itty = children->begin(); itty != children->end(); ++itty ) {
+        ShinyMetaNode * child = fs->findNode(*itty);
+        if( child) {
+            for( uint64_t i=0; i<depth; ++i )
+                printf("  ");
+            printf("[%llu] %s\n", this->weightMap[*itty], child->getName() );
+            if( child->getNodeType() == SHINY_NODE_TYPE_DIR || child->getNodeType() == SHINY_NODE_TYPE_ROOTDIR )
+                printWeightMap( (ShinyMetaDir *)child, depth + 1 );
+        }
+    }
+}
+
+const std::list<inode_t> * ShinyPartitioner::getOurRegion( void ) {
+    return this->ourRegion_final;
+}
+
+const std::list<ShinyPeer *> * ShinyPartitioner::getOurChildren( void ) {
+    return this->ourChildren_final;
+}
+
+const std::map< std::list<inode_t> *, std::list<ShinyPeer *> > * ShinyPartitioner::getRegionPeers( void ) {
+    return this->regionPeers_final;
+}
--- shared/protocol/ShinyPartitioner.h
+++ shared/protocol/ShinyPartitioner.h
@@ -8,52 +8,54 @@
 #include "../filesystem/ShinyMetaDir.h"
 #include "../filesystem/ShinyMetaFilesystem.h"
 
-/* partitionRegion:
-    Takes in number of peers available, spits out all peers assigned to each region, where a region is
-    the set of all inodes strictly under inodes in the root region.
-*/
+//std::map< std::list<inode_t> *, std::list<ShinyPeer *> *> * partitionRegion( ShinyMetaFilesystem * fs, std::list<inode_t> * rootRegion, std::list<ShinyPeer *> * peers, ShinyPeer * us, std::map<inode_t, uint64_t> * weightMap = NULL );
 
-//
-//the set of all nodes strictly UNDER elements in the rootRegion.
-std::map< std::list<inode_t> *, std::list<ShinyPeer *> *> * partitionRegion( ShinyMetaFilesystem * fs, std::list<inode_t> * rootRegion, std::list<ShinyPeer *> * peers, ShinyPeer * us, std::map<inode_t, uint64_t> * weightMap = NULL );
 
-/*
 class ShinyPartitioner {
 public:
     //Takes in a filesystem, and a list of peers to partition across.
     //Our local "root" is the dir over which we have control, and which we will start partitioning
     //peers should be sorted in order of desireability.  Sort by ping, randomly, whatever
-    ShinyPartitioner( ShinyMetaFilesystem *fs, std::list<inode_t> *rootRegion, std::list<ShinyPeer *> *peers );
+    ShinyPartitioner( ShinyMetaFilesystem *fs, std::list<inode_t> *rootRegion, std::list<ShinyPeer *> *peers, ShinyPeer * us );
     
-    //Prints out a weight map
+    ~ShinyPartitioner();
+    
+    //Returns the union of all regions for which we and we alone are responsible
+    const std::list<inode_t> * getOurRegion( void );
+    
+    //Returns the set of peers directly underneath us (just the front()'s of all regions)
+    const std::list<ShinyPeer *> * getOurChildren( void );
+    
+    //Returns a map mapping regions to lists of peers
+    const std::map< std::list<inode_t> *, std::list<ShinyPeer *> > * getRegionPeers( void );
+    
+    //Prints out a weight map starting at root
     void printWeightMap( ShinyMetaDir *root, uint64_t depth = 0 );
 private:
     //Builds a weight map for root, recursing on dirs
     void buildWeightMap( ShinyMetaDir *root );
     
     //Partitions the given region of the weight map amongst the given list of peers
-    void partitionWeightMap( std::list<inode_t> *region, std::list<ShinyPeer *> *peers );
+    std::map< std::list<inode_t> *, std::list<ShinyPeer *> > * partitionRegion( std::list<inode_t> *region, std::list<ShinyPeer *> *peers, uint64_t initialWeight = 0 );
+    
+    
+    //Convenience handle to the actual fs object
+    ShinyMetaFilesystem * fs;
+    
+    //Convenience pointer to "us"
+    ShinyPeer * us;
     
     //This maps goes from a list of inodes, (a "region") to the list of peers assigned to that
-    //region.  The individual entries of these 
-    std::map< std::list<inode_t> *, std::list<ShinyPeer *> > regions;
-    
-    
-    
-    
-    
-    
-    ShinyMetaFilesystem * fs;
+    //region.  The first element in the list is the "owner" of that region.
+    std::map< std::list<inode_t> *, std::list<ShinyPeer *> > * regionPeers_final;
+    std::list<inode_t> * ourRegion_final;
+    std::list<ShinyPeer *> * ourChildren_final;
     
     //Maps inodes to weights
     std::map<inode_t, uint64_t> weightMap;
     
-    //Maps peers to the weights of what they've been assigned
-    std::map<ShinyPeer *, uint64_t> assignedWeights;
-    std::map<ShinyPeer *, std::list<inode_t> > peerAssignments;
-    
-    uint64_t weightPerNode;
+    //Ideal weight per peer
+    uint64_t weightPerPeer;
 };
-*/
 
 #endif
